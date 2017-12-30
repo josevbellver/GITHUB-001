bc1861e7e2dfd0133811930463a8bc054b61265a
josevbellver <josevbellver@gmail.com>
2017-12-30 22:00:52 +0100

    Add files via upload

---
+++ part 01. variables/01-HolaMon/src/pkg01/holamon/HolaMon.java
@@ -0,0 +1,21 @@
+
+package pkg01.holamon;
+
+/**
+ *
+ * @author vicent
+ */
+public class HolaMon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.print("Hola\n\n\n");
+        System.out.print("Hola\n");
+        System.out.print("Hola\n");
+        System.out.println("Hola");
+    }
+    
+}
